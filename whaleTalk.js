16d59d6818307450b08811c9e01ed08ee6c2b0c5
Adam Cox <73854883+AdamCox15@users.noreply.github.com>
2022-03-07 13:10:43 -0700

    Fixed an issue by creating a resultString var

--- whaleTalk.js
+++ whaleTalk.js
@@ -1,20 +1,18 @@
-const input = 'Belgian Malinois are crazy athletic';
+const input = 'I am still the greatest and I am pretty - Bone Thugs n Harmony';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 for (let i = 0; i < input.length; i++) {
   for (let j = 0; j < vowels.length; j++) {
-   if (input[i] === vowels[j]) {
-      if (input[i] === 'e') {
-       resultArray.push('ee');
-     }
-     else if (input[i] === 'u') {
-       resultArray.push('uu');
-     } 
-     else {
-       resultArray.push(input[i]);
-     }
-   }
+  if(input[i] === vowels[j]) {
+    if(input[i] === 'e') {
+      resultArray.push('ee')
+    } else if(input[i] === 'u') {
+      resultArray.push('uu');
+    } else {
+      resultArray.push(input[i]);
+    }
   }
- }
- 
- console.log(resultArray.join('').toUpperCase());
+  }
+}
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
