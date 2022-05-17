35da0cbce7e52943d8c294c82847c67b4d2f8f86
Adam Cox <adamcox.dev@gmail.com>
2022-05-17 11:15:46 -0600

    added a second whale talk, different this time
    did the whale talk project on codecademy again and coded it in a different way getting the same result

--- whaleTalk.js
+++ whaleTalk.js
@@ -16,3 +16,25 @@ for (let i = 0; i < input.length; i++) {
 }
 const resultString = resultArray.join('').toUpperCase();
 console.log(resultString);
+
+
+
+const input = 'Conner McDavid is the best hockey player in the world!';
+const vowels = ['a', 'e', 'i', 'o','u'];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+  if(input[i] === 'e') {
+  resultArray.push('e');
+  }
+  if(input[i] === 'u'){
+    resultArray.push('u');
+  }
+for (let j = 0; j < vowels.length; j++){
+  if(input[i] === vowels[j]){
+resultArray.push(input[i]); 
+}
+}
+} 
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
